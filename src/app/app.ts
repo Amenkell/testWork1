2c5c7b4f6143b7f4b24f29a8c682201f22f3617e
Amenkell <win.kudymov@gmail.com>
2025-08-07 21:59:32 +0300

    Отрефакторил код

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,11 @@
-import {ChangeDetectionStrategy, Component, effect, inject, OnInit, signal, WritableSignal} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  signal,
+  WritableSignal
+} from '@angular/core';
 import {UserService} from '../services/user.service';
 import {FormsModule} from '@angular/forms';
 import {StatusTypeEnum} from '../models/status-type.enum';
@@ -11,50 +18,41 @@ import {User} from '../models/user.interface';
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class App implements OnInit {
-
-  protected users: User[] = [];
-  protected filteredUsers: WritableSignal<User[]> = signal<User[]>([]);
-  protected searchName: WritableSignal<string> = signal('');
-  protected filterStatus: WritableSignal<StatusTypeEnum> = signal<StatusTypeEnum>(StatusTypeEnum.ALL);
-  protected selectedUser: WritableSignal<User | null> = signal<User | null>(null);
-  protected statusTypes: WritableSignal<string[]> = signal(Object.values(StatusTypeEnum));
+export class App {
 
   private userService: UserService = inject(UserService);
+  protected searchName: WritableSignal<string> = signal('');
+  protected filterStatus: WritableSignal<StatusTypeEnum> = signal<StatusTypeEnum>(StatusTypeEnum.ALL);
+  protected statusTypes: WritableSignal<string[]> = signal(Object.values(StatusTypeEnum));
+  protected selectedUserSignal = signal<User | null>(null);
+  private usersSignal = this.userService.getUsers();
 
-  constructor() {
-    effect(() => {
+  protected filteredUsers = computed(() => {
+    const searchVal = this.searchName().toLowerCase();
+    const filterVal = this.filterStatus();
+    let filtered = this.usersSignal();
 
-      const searchVal = this.searchName().toLowerCase();
-      const filterVal = this.filterStatus();
-      let filteredUsers: User[] = this.users;
+    if (filterVal === StatusTypeEnum.ACTIVE) {
+      filtered = filtered.filter(u => u.active);
+    } else if (filterVal === StatusTypeEnum.INACTIVE) {
+      filtered = filtered.filter(u => !u.active);
+    }
 
-      if (filterVal === StatusTypeEnum.ACTIVE) {
-        filteredUsers = filteredUsers.filter((u) => u.active);
-      } else if (filterVal === StatusTypeEnum.INACTIVE) {
-        filteredUsers = filteredUsers.filter((u) => !u.active);
-      }
+    if (searchVal) {
+      filtered = filtered.filter(u => u.name.toLowerCase().includes(searchVal));
+    }
 
-      if (searchVal) {
-        filteredUsers = filteredUsers.filter((u) => u.name.toLowerCase().includes(searchVal));
-      }
+    return filtered;
+  });
 
-      this.filteredUsers.set(filteredUsers);
+  protected selectedUser = computed(() => {
+    const current = this.selectedUserSignal();
+    const filtered = this.filteredUsers();
 
-      if (!filteredUsers.some((u) => u.id === this.selectedUser()?.id)) {
-        this.selectedUser.set(null);
-      }
-    });
-  }
-
-  public ngOnInit(): void {
-
-    this.users = this.userService.getUsers()();
-    this.filteredUsers.set(this.users);
-
-  }
+    return filtered.some(u => u.id === current?.id) ? current : null;
+  });
 
   selectUser(user: User): void {
-    this.selectedUser.set(user);
+    this.selectedUserSignal.set(user);
   }
 }
